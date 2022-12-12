8e04f500ad37f4e21cd0344473fb08988ca793fe
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-12-12 11:36:52 +0000

    added button, fixed reactDOM import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import Button from "./Button";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+    </div>
+  );
 }
 export default App;

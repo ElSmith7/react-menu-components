5e1b4271e0e42dbc8763e137da48a1d8c4abe760
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-12-12 16:09:56 +0000

    imported classnames and react-icons.  Created special Button component styling and overall functionality for other devs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import Button from "./Button";
+import { GoBell, GoCloudDownload, GoEye } from "react-icons/go";
 
 function App() {
+  const handleClick = () => {
+    console.log("hi");
+  };
   return (
     <div>
       <div>
-        <Button success outline>
+        <Button success outline onClick={handleClick} className="mb-5">
+          <GoBell />
           Hello!
         </Button>
       </div>
       <div>
         <Button primary rounded>
+          <GoCloudDownload />
           Buy Now
         </Button>
       </div>
       <div>
-        <Button danger>See Deal</Button>
+        <Button danger>
+          <GoEye />
+          See Deal
+        </Button>
       </div>
       <div>
         <Button secondary outline rounded>

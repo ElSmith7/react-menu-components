1eb1cdcecbb9c13938b310285d3dce5bb32b2b54
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-12-20 14:34:18 +0000

    added state to impliment opening and closing of menu onClick

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function DropDown({ options }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  };
+
   const renderedOptions = options.map((option) => {
     return <div key={option.value}>{option.label}</div>;
   });
-  return <div>{renderedOptions}</div>;
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}{" "}
+    </div>
+  );
 }
 
 export default DropDown;

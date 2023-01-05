94f788a5347a2436ed8f6f5ca84a3cc3c62174a3
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-01-05 14:25:23 +0000

    moved dropdown from app to dropdown page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,5 @@
-import { useState } from "react";
-import DropDown from "./components/DropDown";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      label: "Red",
-      value: "red",
-    },
-    {
-      label: "Green",
-      value: "green",
-    },
-    {
-      label: "Blue",
-      value: "blue",
-    },
-  ];
-  return (
-    <div className="flex">
-      <DropDown value={selection} onChange={handleSelect} options={options} />
-      <DropDown value={selection} onChange={handleSelect} options={options} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

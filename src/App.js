2077ca3fdc44bc166c3a8da04372e8bb3f307efb
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2023-01-09 15:36:11 +0000

    Added components to App and accessible links, created navigation context to disable and handle default loading

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from "./pages/AccordionPage";
+import DropDownPage from "./pages/DropDownPage";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/accordion">Go to Accordion</Link>
+      <Link to="/dropdown">Go to DropDown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropDownPage />
+        </Route>
+      </div>
+    </div>
+  );
 }
 
 export default App;
